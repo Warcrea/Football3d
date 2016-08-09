0dfe3cfd8f4df759270212e00ddeca5c7f9cdc48
Andrew Stockdale <as834@kent.ac.uk>
2016-08-09 15:05:57 +0100

    Kick Particles
    Basic kick particles working and the ball now slides to the player's
    feet when dribbling

---
+++ Football3d/Assets/Scripts/Movement/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public bool canBeDribbled = true;
+    private float dribbleTimerReset = 0.5f;
+    private float dribbleTimer;
+
+    public GameObject beingDribbledBy;
+    public float floorYCoord = 0.28f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dribbleTimer != 0) {
+            canBeDribbled = false;
+            dribbleTimer = Mathf.Max(dribbleTimer - Time.deltaTime, 0);
+        }
+        else canBeDribbled = true;
+
+        if (beingDribbledBy != null) {
+            Vector3 correctedYVector = transform.position;
+            correctedYVector.y = Mathf.Max(transform.position.y - 0.02f, floorYCoord);
+            transform.position = correctedYVector;
+        }
+	}
+
+    public void beginDribbling(GameObject player) {
+        beingDribbledBy = player;
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    public void beShot() {
+        dribbleTimer = dribbleTimerReset;
+        beingDribbledBy = null;
+        gameObject.GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+}
